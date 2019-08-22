16b5cbecfb56218c629dfd5a91df55623f94b9af
OPivv13 <oranong32699@gmail.com>
2019-08-23 02:01:08 +0700

    bookCar click and show II

--- app/src/main/java/buu/informatics/cs59160395/bookcar/MainActivity.kt
+++ app/src/main/java/buu/informatics/cs59160395/bookcar/MainActivity.kt
@@ -38,12 +38,12 @@ class MainActivity : AppCompatActivity() {
 
         // click edit
         findViewById<Button>(R.id.edit_button).setOnClickListener {
-
+            showCreate(it, sl)
         }
 
         // click delete
         findViewById<Button>(R.id.delete_button).setOnClickListener {
-
+            showHome(it)
         }
     }
 
@@ -101,6 +101,9 @@ class MainActivity : AppCompatActivity() {
         val slot1 = findViewById<TextView>(R.id.slot_one_text)
         val slot2 = findViewById<TextView>(R.id.slot_two_text)
         val slot3 = findViewById<TextView>(R.id.slot_three_text)
+        val numberText = findViewById<TextView>(R.id.number_car_text)
+        val brandText = findViewById<TextView>(R.id.brand_car_text)
+        val nameText = findViewById<TextView>(R.id.name_owner_text)
         val labelColor1 = findViewById<TextView>(R.id.null_color_text)
         val label1 = findViewById<TextView>(R.id.null_text)
         val labelColor2 = findViewById<TextView>(R.id.unull_color_text)
@@ -109,12 +112,27 @@ class MainActivity : AppCompatActivity() {
         val brandEdit = findViewById<EditText>(R.id.brand_car_edit)
         val nameEdit = findViewById<EditText>(R.id.name_owner_edit)
         val updateBtn = findViewById<Button>(R.id.update_button)
+        val homeBtn = findViewById<Button>(R.id.home_button)
+        val editBtn = findViewById<Button>(R.id.edit_button)
+        val deleteBtn = findViewById<Button>(R.id.delete_button)
 
-        // hide label
+        // hide label textView and button
         labelColor1.visibility = View.GONE
         label1.visibility = View.GONE
         labelColor2.visibility = View.GONE
         label2.visibility = View.GONE
+        homeBtn.visibility = View.GONE
+        editBtn.visibility = View.GONE
+        deleteBtn.visibility = View.GONE
+        numberText.visibility = View.GONE
+        brandText.visibility = View.GONE
+        nameText.visibility = View.GONE
+
+        // show editText and button
+        updateBtn.visibility = View.VISIBLE
+        numberEdit.visibility = View.VISIBLE
+        brandEdit.visibility = View.VISIBLE
+        nameEdit.visibility = View.VISIBLE
 
         // keep num slot
         sl = num
@@ -137,12 +155,6 @@ class MainActivity : AppCompatActivity() {
                 slot3.visibility = View.VISIBLE
             }
         }
-
-        // show editText and button
-        numberEdit.visibility = View.VISIBLE
-        brandEdit.visibility = View.VISIBLE
-        nameEdit.visibility = View.VISIBLE
-        updateBtn.visibility = View.VISIBLE
 
     }
 
