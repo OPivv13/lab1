2702c176fe0473a77cafb7c8bb53f1e627a9a7ff
OPivv13 <oranong32699@gmail.com>
2019-08-24 02:01:46 +0700

    bookCar Code I

--- app/src/main/java/buu/informatics/cs59160395/bookcar/MainActivity.kt
+++ app/src/main/java/buu/informatics/cs59160395/bookcar/MainActivity.kt
@@ -10,11 +10,15 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import buu.informatics.cs59160395.bookcar.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var slotValue: Int = 0
-//    private val slot: Slot = Slot(slotValue, "", "", "")
+    private var slot0: Slot = Slot(0, "", "", "")
+    private var slot1: Slot = Slot(1, "", "", "")
+    private var slot2: Slot = Slot(2, "", "", "")
+    private var slot3: Slot = Slot(3, "", "", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +27,20 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             // click slot
             slotOneText.setOnClickListener {
-                showCreate(it, 1)
+                slotValue = 1
+                checkSlot(it)
             }
             slotTwoText.setOnClickListener {
-                showCreate(it, 2)
+                slotValue = 2
+                checkSlot(it)
             }
             slotThreeText.setOnClickListener {
-                showCreate(it, 3)
+                slotValue = 3
+                checkSlot(it)
             }
             // click update
             updateButton.setOnClickListener {
-                addCar(it, slotValue)
+                addSlot(it)
             }
             // click home
             homeButton.setOnClickListener {
@@ -41,23 +48,105 @@ class MainActivity : AppCompatActivity() {
             }
             // click edit
             editButton.setOnClickListener {
-                showCreate(it, slotValue)
+                showEdit(it, false)
             }
             // click delete
             deleteButton.setOnClickListener {
-                showHome(it)
+                deleteSlot(it)
             }
 
-//            slot = this@MainActivity.slot
+            slot = this@MainActivity.slot1
+            slot = this@MainActivity.slot2
+            slot = this@MainActivity.slot3
+            slot = this@MainActivity.slot0
         }
 
     }
 
-    private fun addCar(view: View, num: Int){
+    private fun checkSlot(view: View){
         binding.apply {
-            // add slot num brand and name to data
-            // ????????????????
+            when (slotValue) {
+                1 -> {
+                    if (slot1.numberCar == ""){
+                        showEdit(view, true)
+                    } else {
+                        showSlot(view)
+                    }
+                }
+                2 -> {
+                    if (slot2.numberCar == ""){
+                        showEdit(view, true)
+                    } else {
+                        showSlot(view)
+                    }
+                }
+                3 -> {
+                    if (slot3.numberCar == ""){
+                        showEdit(view, true)
+                    } else {
+                        showSlot(view)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun showSlot(view: View){
+        binding.apply{
+            // hide
+            numberCarEdit.visibility = View.GONE
+            brandCarEdit.visibility = View.GONE
+            nameOwnerEdit.visibility = View.GONE
+            updateButton.visibility = View.GONE
+            nullColorText.visibility = View.GONE
+            nullText.visibility = View.GONE
+            unullColorText.visibility = View.GONE
+            unullText.visibility = View.GONE
+
+            // show
+            numberCarText.visibility = View.VISIBLE
+            brandCarText.visibility = View.VISIBLE
+            nameOwnerText.visibility = View.VISIBLE
+            homeButton.visibility = View.VISIBLE
+            editButton.visibility = View.VISIBLE
+            deleteButton.visibility = View.VISIBLE
+
+            // show slot and hide slot
+            when (slotValue) {
+                1 -> {
+                    slotOneText.visibility = View.VISIBLE
+                    slotTwoText.visibility = View.GONE
+                    slotThreeText.visibility = View.GONE
+
+                    numberCarText.text = slot1.numberCar
+                    brandCarText.text = slot1.brandCar
+                    nameOwnerText.text = slot1.nameOwner
+                }
+                2 -> {
+                    slotOneText.visibility = View.GONE
+                    slotTwoText.visibility = View.VISIBLE
+                    slotThreeText.visibility = View.GONE
+
+                    numberCarText.text = slot2.numberCar
+                    brandCarText.text = slot2.brandCar
+                    nameOwnerText.text = slot2.nameOwner
+                }
+                3 -> {
+                    slotOneText.visibility = View.GONE
+                    slotTwoText.visibility = View.GONE
+                    slotThreeText.visibility = View.VISIBLE
+
+                    numberCarText.text = slot3.numberCar
+                    brandCarText.text = slot3.brandCar
+                    nameOwnerText.text = slot3.nameOwner
+                }
+            }
+            invalidateAll()
+        }
+    }
+
+    private fun addSlot(view: View){
+        binding.apply {
             // hide editText and button
             numberCarEdit.visibility = View.GONE
             brandCarEdit.visibility = View.GONE
@@ -72,22 +161,30 @@ class MainActivity : AppCompatActivity() {
             editButton.visibility = View.VISIBLE
             deleteButton.visibility = View.VISIBLE
 
-            // check slot and show slot
-            when (num) {
+            // add slot
+            when (slotValue) {
                 1 -> {
-                    slotOneText.visibility = View.VISIBLE
-                    slotTwoText.visibility = View.GONE
-                    slotThreeText.visibility = View.GONE
+                    // add slot num brand and name to data
+                    slot1.numberCar = numberCarEdit.text.toString()
+                    slot1.brandCar = brandCarEdit.text.toString()
+                    slot1.nameOwner = nameOwnerEdit.text.toString()
+//                    slot = this@MainActivity.slot1
+
                 }
                 2 -> {
-                    slotOneText.visibility = View.GONE
-                    slotTwoText.visibility = View.VISIBLE
-                    slotThreeText.visibility = View.GONE
+                    // add slot num brand and name to data
+                    slot2.numberCar = numberCarEdit.text.toString()
+                    slot2.brandCar = brandCarEdit.text.toString()
+                    slot2.nameOwner = nameOwnerEdit.text.toString()
+//                    slot = this@MainActivity.slot2
+
                 }
-                else -> {
-                    slotOneText.visibility = View.GONE
-                    slotTwoText.visibility = View.GONE
-                    slotThreeText.visibility = View.VISIBLE
+                3 -> {
+                    // add slot num brand and name to data
+                    slot3.numberCar = numberCarEdit.text.toString()
+                    slot3.brandCar = brandCarEdit.text.toString()
+                    slot3.nameOwner = nameOwnerEdit.text.toString()
+//                    slot = this@MainActivity.slot3
                 }
             }
 
@@ -99,8 +196,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showCreate(view: View, num: Int){
+    private fun showEdit(view: View, checkNull: Boolean){
         binding.apply {
+            if(checkNull == true){
+                // clean editText
+                numberCarEdit.text.clear()
+                brandCarEdit.text.clear()
+                nameOwnerEdit.text.clear()
+            }
             // hide label textView and button
             nullColorText.visibility = View.GONE
             nullText.visibility = View.GONE
@@ -119,28 +222,31 @@ class MainActivity : AppCompatActivity() {
             brandCarEdit.visibility = View.VISIBLE
             nameOwnerEdit.visibility = View.VISIBLE
 
-            // keep num slot
-            slotValue = num
-
-            // check slot and show slot
-            when (num) {
+            // show slot and hide slot
+            when (slotValue) {
                 1 -> {
                     slotOneText.visibility = View.VISIBLE
                     slotTwoText.visibility = View.GONE
                     slotThreeText.visibility = View.GONE
+
                 }
                 2 -> {
                     slotOneText.visibility = View.GONE
                     slotTwoText.visibility = View.VISIBLE
                     slotThreeText.visibility = View.GONE
+
+
                 }
                 3 -> {
                     slotOneText.visibility = View.GONE
                     slotTwoText.visibility = View.GONE
                     slotThreeText.visibility = View.VISIBLE
+
+
                 }
             }
 
+            invalidateAll()
             numberCarEdit.requestFocus()
             // show the keyboard
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -150,6 +256,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun showHome(view: View){
         binding.apply {
+
             // show slot and label
             slotOneText.visibility = View.VISIBLE
             slotTwoText.visibility = View.VISIBLE
@@ -169,6 +276,32 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+    }
+
+    private fun deleteSlot(view: View){
+        binding.apply{
+            // change value slot
+            when (slotValue) {
+                1 -> {
+                    slot1.numberCar = ""
+                    slot1.brandCar = ""
+                    slot1.nameOwner = ""
+                }
+                2 -> {
+                    slot2.numberCar = ""
+                    slot2.brandCar = ""
+                    slot2.nameOwner = ""
+                }
+                3 -> {
+                    slot3.numberCar = ""
+                    slot3.brandCar = ""
+                    slot3.nameOwner = ""
+                }
+            }
+
+            showHome(view)
+        }
 
     }
 }
